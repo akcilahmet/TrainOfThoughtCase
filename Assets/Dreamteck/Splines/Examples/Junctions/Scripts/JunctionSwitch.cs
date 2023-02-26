ca92a47ad641611c9168fca18de4ec00eb1fa844
Ahmet Akçıl <83040124+akcilahmet@users.noreply.github.com>
2023-02-27 02:00:07 +0300

    dreamteck assets added

---
+++ Assets/Dreamteck/Splines/Examples/Junctions/Scripts/JunctionSwitch.cs
@@ -0,0 +1,51 @@
+namespace Dreamteck.Splines.Examples
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class JunctionSwitch : MonoBehaviour
+    {
+        [System.Serializable]
+        public class Bridge
+        {
+            public enum Direction { Forward = 1, Backward = -1, None = 0 }
+            public bool active = true;
+            public int a;
+            public Direction aDirection = Direction.None;
+            public int b;
+            public Direction bDirection = Direction.None;
+        }
+
+        public Bridge[] bridges;
+
+        private void OnValidate()
+        {
+            Node node = GetComponent<Node>();
+            Node.Connection[] connections = node.GetConnections();
+            if (bridges == null) return;
+            for (int i = 0; i < bridges.Length; i++)
+            {
+                if (bridges[i].a < 0) bridges[i].a = 0;
+                if (bridges[i].b < 0) bridges[i].b = 0;
+                if (bridges[i].a >= connections.Length) bridges[i].a = connections.Length - 1;
+                if (bridges[i].b >= connections.Length) bridges[i].b = connections.Length - 1;
+            }
+        }
+
+        public void SwitchActive()
+        {
+            bridges[0].active = true;
+            bridges[0].a = 0;
+            bridges[0].b = 1;
+            bridges[0].aDirection = Bridge.Direction.Forward;
+        } 
+        public void SwitchReset()
+        {
+            bridges[0].active = false;
+            bridges[0].a = 0;
+            bridges[0].b = 0;
+            bridges[0].aDirection = Bridge.Direction.None;
+        }
+    }
+}
